65aff648e96482e4086edf4f2008510b774c1006
Ajay Chauhan <ajaychauhanpp44@gmail.com>
2025-07-06 09:19:42 +0530

    Solve Five Question of Pattern

---
+++ 04_star_pattern/index.js
@@ -0,0 +1,119 @@
+
+// Question 1
+/*
+  ****
+  ****
+  ****
+  ****
+*/
+let n=4 ;
+  for(let i = 0 ; i < n ; i++){
+     let row =""
+      for(let j = 0 ; j < n ; j ++){
+        row +="*";
+      }
+    //   console.log(row)
+  }
+
+  
+// Question 2
+/*
+  *
+  * *
+  * * *
+  * * *  *
+*/
+ let n2 =10; 
+    for(let i = 0 ; i <  n2; i ++){
+        
+        let star =" ";
+        for(let j=0 ; j <= i ; j ++){
+             star += " *"
+        }
+        //  console.log(star)
+    }
+// Question 3
+/*
+  1
+  1 2
+  1 2 3 
+  1 2 3 4 
+  1 2 3 4 5
+*/
+//  let n3 = 5; 
+//     for(let i = 1 ; i <=  n3; i ++){
+//           let number = ""
+//         for(let j=1 ; j <= i ; j ++){ 
+//              number += ` ${j}`            
+//         }
+//          console.log(number)
+//     }
+
+// Question 4 
+// 1
+// 2 2
+// 3 3 3
+// 4 4 4 4
+// 5 5 5 5 5
+
+let n4 = 5;
+
+// for(let i = 0 ; i < n4 ; i++){
+//     let row =" "
+//       for(let j = 0 ; j <= i ; j++){
+//          row += i+1
+//       }
+//        console.log(row)
+// }
+
+for(let i = 1 ; i <= n4 ; i++){
+    let row =" "
+      for(let j = 1 ; j <= i ; j++){
+         row += ` ${i}`
+      }
+    //    console.log(row)
+}
+
+//   Question -5 
+/*
+  1 2 3 4 5
+  1 2 3 4
+  1 2 3 
+  1 2
+  1
+*/
+let n5 = 5;
+
+//  for(let i = n5 ; i > 0 ; i--){
+//     let rownumber =""
+//       for(let j = 1 ; j <= i ; j ++){
+//         rownumber += ` ${j}`
+//       }
+
+//        console.log(rownumber)
+//  }
+
+
+// for(let i = 0 ; i < n5 ; i++){
+//     let rownumber = "";
+//      for(let  j = 0 ; j < n5 - i; j ++){
+//         rownumber += ` ${j+1}`
+//      }
+//       console.log(rownumber)
+
+// }
+
+//   Question -6
+
+
+/* 
+   _ _ _ _ *
+   _ _ _ * *
+   - - * * *
+   - * * * *
+   * * * * *
+*/
+ let n6 = 5;
+for( let i =0 ; i < n6; i++){
+    
+}
