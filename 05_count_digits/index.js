e00c31724df0ada03a61363ef6e362bc092c1aa8
Ajay Chauhan <ajaychauhanpp44@gmail.com>
2025-07-08 08:04:29 +0530

    Solve count of digits problem

---
+++ 05_count_digits/index.js
@@ -0,0 +1,32 @@
+// console.log("Count Digitssss")
+
+
+// Q -1 
+
+ //let numbers= 259  Output = 3;
+
+   function  findNumberofDigits (numbers){
+     let str = String(numbers);
+      const splitnum =  str.split("");
+     return splitnum.length;
+
+   }
+    let numbers= 6234567890
+    // const result = findNumberofDigits(numbers);
+    //  console.log(result)
+
+
+    function  countDigits(n) {
+         if(n === 0) return 1;
+         n = Math.abs(n);
+         let count = 0;
+          while( n > 0 ){
+             n = Math.floor(n / 10);
+              count ++;
+          }
+           return count;
+
+        
+    }
+    const result =  countDigits(numbers);
+     console.log(result);
