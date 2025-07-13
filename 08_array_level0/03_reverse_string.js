567b772631a680b9a60d469fd594a54e9fadb906
Ajay Chauhan <ajaychauhanpp44@gmail.com>
2025-07-13 09:55:56 +0530

    Slove Reverse String , modified the current elements of an array

---
+++ 08_array_level0/03_reverse_string.js
@@ -0,0 +1,27 @@
+
+function reverseString (s) {
+    //  console.log(s)
+    let revString = "";
+    //  }
+     for( let i= s.length - 1 ; i >=0 ; i--){
+         revString  += s[i] 
+     }
+      return revString.split("")
+
+}    
+let s = ["h","e","l","l","o"]
+//  console.log(reverseString(s))
+
+var reverseString2 = function(s) {
+     let  len = s.length;
+      let  half = Math.floor(len/2);
+     for( let i= 0 ; i < half ; i++){
+            let temp = s[i];
+            s[i] = s[len - 1 - i]
+           s[len - 1 - i] = temp
+     }
+     return s 
+
+};
+
+ console.log(reverseString2(s))
